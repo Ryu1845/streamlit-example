b159c1dbf34a819b47aa7ad95cec029f84e12686
Ryu1845 <ryu@tpgjbo.xyz>
2022-06-06 19:00:59 +0200

    First actual commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,5 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from twspace_dl import Twspace, TwspaceDL
 
 """
 # Welcome to Streamlit!
@@ -15,24 +12,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+st.set_page_config(page_title="twspace-dl online", page_icon="🌟", layout="wide")
+params = st.experimental_get_query_params()
+url = st.text_input(label="Space URL")
+space = Twspace.from_space_url(url)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with st.sidebar:
+    st.write("## Twitter Space Downloader")
